076dd4d85510e5a02ff28429fcba75e1d385ef51
prathyusha-dev34 <prathyushak795@gmail.com>
2026-03-11 22:20:43 +0530

    Add files via upload

---
+++ App Development.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppDevelopment() {
+  return <h3>We build Android and iOS applications.</h3>;
+}
+
+export default AppDevelopment;
---
+++ UiUx.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UiUx() {
+  return <h3>We design modern UI/UX experiences.</h3>;
+}
+
+export default UiUx;
